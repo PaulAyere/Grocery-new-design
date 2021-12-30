818bd428f5b8a38449dbaab9e178e79f779d0fdb
Paulinus Ayere Aghughu <paulynpen@gmail.com>
2021-12-30 11:28:44 +0100

    added some javascrip code for countdown timer

--- script.js
+++ script.js
@@ -18,3 +18,32 @@ window.onscroll = () =>{
     }
 
 }
+let countDate = new Date('December 20, 2021 00:00:00').getTime();
+
+function CountDown(){
+
+    let now = new Date().getTime();
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap / (day));
+    let h = Math.floor((gap % (day)) / (hour));
+    let m = Math.floor((gap % (hour)) / (minute));
+    let s = Math.floor((gap % (minute)) / (second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+
+    console.log(gap)
+
+}
+
+setInterval(function(){
+    CountDown();
+},1000)
